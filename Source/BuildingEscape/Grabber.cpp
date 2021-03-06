0f33171a76b0110f256647da3889088a36fb1b1a
Gokhan Irmak <gokhanirmak2@hotmail.com>
2021-03-06 16:45:12 +0300

    LineTracing And RayCasting Coded

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,10 @@
 // Copyright GokhanIrmak 2021
 
-
 #include "Grabber.h"
+#include "GameFramework/PlayerController.h"
+#include "DrawDebugHelpers.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -13,23 +16,59 @@ UGrabber::UGrabber()
 	// ...
 }
 
-
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp,Warning,TEXT("Grabber Reporting..."));
+	UE_LOG(LogTemp, Warning, TEXT("Grabber Reporting..."));
 	// ...
-	
 }
 
-
 // Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
-}
+	//Get Players viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation);
 
+	// UE_LOG(LogTemp, Warning,
+	// 	   TEXT("Player Viewpoint Location: %s\nPlayer ViewpointRotation: %s"),
+	// 	   *PlayerViewPointLocation.ToString(),
+	// 	   *PlayerViewPointRotation.ToString());
+
+	//Draw a line from player showing the reach
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor::Green,
+		false,
+		0.f,
+		0,
+		5.f);
+	//Ray-cast out to a certain distance (Reach)
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams);
+
+	if (Hit.GetActor())
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s"), *(Hit.GetActor()->GetName()));
+	}
+	//See what it hits
+}
